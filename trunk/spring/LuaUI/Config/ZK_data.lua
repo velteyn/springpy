3513fe277cd2923b568153b67cade5881abe2c78
kingraptorowns@gmail.com <kingraptorowns@gmail.com@341e8072-d98e-f3fb-eb89-a7e1584d0b14>
2013-06-13 15:01:49 +0000

    Functionality for Spring passing combat results to Renpy (example widget provided) Also add Spring engine files.

---
+++ trunk/spring/LuaUI/Config/ZK_data.lua
@@ -0,0 +1,91 @@
+-- Widget Custom Data
+return {
+	version = 9,
+	["Auto Group"] = {
+		version = "3.031",
+		groups = {},
+	},
+	["Chili Docking"] = {
+		Chat = {
+			[1] = 551.79998779297,
+			[2] = 0,
+			[3] = 979,
+			[4] = 172.40000915527,
+		},
+		Minimap = {
+			[1] = 0,
+			[2] = 0,
+			[3] = 328,
+			[4] = 266,
+		},
+		["Player List"] = {
+			[1] = 1128,
+			[2] = 712,
+			[3] = 1424,
+			[4] = 862,
+		},
+		ResourceBars = {
+			[1] = 994,
+			[2] = 0,
+			[3] = 1424,
+			[4] = 50,
+		},
+		epicmenubar = {
+			[1] = 979,
+			[2] = 50,
+			[3] = 1424,
+			[4] = 100,
+		},
+		integralwindow = {
+			[1] = 0,
+			[2] = 682,
+			[3] = 450,
+			[4] = 862,
+		},
+		pwinfo = {
+			[1] = 1204,
+			[2] = 172,
+			[3] = 1424,
+			[4] = 280,
+		},
+		real_window_corner = {
+			[1] = 0,
+			[2] = 552,
+			[3] = 450,
+			[4] = 682,
+		},
+		selector_window = {
+			[1] = 450,
+			[2] = 810,
+			[3] = 834,
+			[4] = 862,
+		},
+	},
+	CustomFormations2 = {
+		maxHungarianUnits = 20,
+	},
+	["EPIC Menu"] = {
+		country = "my",
+		lang = "en",
+		music_volume = 0.5,
+		show_crudemenu = true,
+		sub_pos_x = 712,
+		sub_pos_y = 431,
+		versionmin = 50,
+		wl_h = 603.39996337891,
+		wl_w = 300,
+		wl_x = 562,
+		wl_y = 129.30001831055,
+		config = {
+			["epic_Settings/Camera_Camera_Type"] = "Total Annihilation",
+		},
+		widgets = {},
+	},
+	["Local Widgets Config"] = {
+		localWidgets = false,
+		localWidgetsFirst = false,
+	},
+	["Persistent Build Spacing"] = {
+		buildSpacing = {},
+	},
+}
---
+++ trunk/spring/result.py
@@ -0,0 +1,1 @@
+gameframe = 80
